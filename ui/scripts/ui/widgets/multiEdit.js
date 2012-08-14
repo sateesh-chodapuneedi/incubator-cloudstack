3b267ae75b3f96507a5820de6fb58fa71455518e
Pranav Saxena <pranav.saxena@citrix.com>
2012-08-14 18:14:45 +0530

    CS-15981:changing addvm's to add

--- ui/scripts/ui/widgets/multiEdit.js
+++ ui/scripts/ui/widgets/multiEdit.js
@@ -812,7 +812,7 @@
           }).appendTo($td);
       } else if (field.addButton) {
         $addVM = $('<div>').addClass('button add-vm').html(
-          _l('label.add.vms')
+          _l('label.add')
         ).appendTo($td);
       }
     });
