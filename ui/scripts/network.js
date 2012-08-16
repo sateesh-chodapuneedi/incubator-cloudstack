57946625ed331ee2caf58106417a7b74e148e560
Pranav Saxena <pranav.saxena@citrix.com>
2012-08-16 17:18:39 +0530

    CS-16004:LB rule name , ports validation missing under the load balancers tab for a basic zone autoscale configuration

--- ui/scripts/network.js
+++ ui/scripts/network.js
@@ -848,6 +848,7 @@
                         'autoScale': {
                           label: 'AutoScale',
                           custom: {
+                            requireValidation: true,
                             buttonLabel: 'label.configure',
                             action: cloudStack.uiCustom.autoscaler(cloudStack.autoscaler)
                           }
