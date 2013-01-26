13d94c14989a340fcc669a2337d5070e592b0d26
Prasanna Santhanam <tsp@apache.org>
2013-01-26 19:25:30 +0530

    fixing the imports
    Signed-off-by: Prasanna Santhanam <tsp@apache.org>

--- tools/marvin/marvin/integration/lib/base/CloudStackEntity.py
+++ tools/marvin/marvin/integration/lib/base/CloudStackEntity.py
@@ -5,27 +5,21 @@
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
-#
+# 
 #   http://www.apache.org/licenses/LICENSE-2.0
-#
+# 
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-from . import CloudStackEntity
-class AutoScaleVmProfile(CloudStackEntity):
 
-    def create(self, apiclient, AutoScaleVmProfileFactory, zoneid, serviceofferingid, templateid, **kwargs):
-        pass
+cloudstack_version = "4.1.0"
 
-    def list(self, apiclient, **kwargs):
-        pass
+class CloudStackEntity(object):
 
-    def update(self, apiclient, id, **kwargs):
-        pass
+    __version__ = cloudstack_version
 
-    def delete(self, apiclient, id, **kwargs):
-        pass
-
+    def getVersion(self):
+        return self.__version__
